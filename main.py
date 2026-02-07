4e5fa07f4c0c362662146aa85655774f08d20ad0
GOST <jbugra23@outlook.es>
2026-02-06 20:03:22 -0500

    Nueva version

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import sys
 
 if __name__ == '__main__':
-    rango = int(sys.argv[1])
+    rango = int(input('Ingresa un valor'))
+    if not rango: rango = 0
     print("Generando un numero aleatorio en un rango de 0 a " + str(rango))
-    print(random.choice(range(rango)))
+    print(random.randint(0,rango))
